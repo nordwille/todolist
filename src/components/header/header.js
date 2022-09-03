087abb6dad70acf13750f4ae3544c299c547ebd8
Misha <nordwille@gmail.com>
2022-09-03 15:13:58 +0300

    hamburger-menu-toggle

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
-import './header.css';
-import LeftControl from './left-control/left-control';
+import "./header.css";
+import LeftControl from "./left-control/left-control";
 
 const Header = () => {
-    return (
-        <header>
-            <LeftControl/>
-        </header>
-    )
-}
+  return (
+    <header>
+      <LeftControl />
+    </header>
+  );
+};
 
 export default Header;
--- src/components/header/left-control/left-control.js
+++ src/components/header/left-control/left-control.js
@@ -1,16 +1,28 @@
 import "./left-control.css";
 import SearchPanel from "./search-panel/search-panel";
+import { Link } from "react-router-dom";
 
 const LeftControl = () => {
+  function showLeftMenu() {
+    const leftMenu = document.querySelector(".left-menu");
+    if (leftMenu.style.display === "block") {
+      leftMenu.style.display = "none";
+    } else {
+      leftMenu.style.display = "block";
+    }
+  }
   return (
     <div className="left-control">
-      <nav className="menu">
+      <nav className="burger-menu-button" onClick={showLeftMenu}>
         <i className="fa-solid fa-bars"></i>
       </nav>
 
-      <nav className="home">
-        <i className="fa-solid fa-house"></i>
-      </nav>
+      <Link to="/today">
+        <nav className="home-button">
+          <i className="fa-solid fa-house"></i>
+        </nav>
+      </Link>
+
       <SearchPanel />
     </div>
   );
